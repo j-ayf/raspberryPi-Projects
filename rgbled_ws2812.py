d3eee5b0e83fad211e49e2085d4c70b43b485b0f
janik <janikfrost@gmail.com>
2022-07-10 12:39:10 +0200

    Add schematics and PIN documentation for RGB LED WS2812

--- rgbled_ws2812.py
+++ rgbled_ws2812.py
@@ -5,19 +5,37 @@
 # Direct port of the Arduino NeoPixel library strandtest example.  Showcases
 # various animations on a strip of NeoPixels.
 
+
+#############################
+#    RGB LED WS2812 Pins    #
+#              #
+#   _____________________   #
+#   |                   |   #
+#   |                   |   #
+#   |                   |   #
+#   |_____ 1 2 3 4 _____|   #
+#                           #
+#   1: DIN (GPIO)           #
+#   2: VCC                  #
+#   3: GND                  #
+#   4: DOUT (next LED)      #
+#                           #
+#############################
+
+
 import time
 from rpi_ws281x import PixelStrip, Color
 import argparse
 
 # LED strip configuration:
-LED_COUNT = 1        # Number of LED pixels.
-LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
+LED_COUNT = 1  # Number of LED pixels.
+LED_PIN = 18  # GPIO pin connected to the pixels (18 uses PWM!).
 # LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA = 10          # DMA channel to use for generating signal (try 10)
+LED_DMA = 10  # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
-LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0  # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 
 # Define functions which animate LEDs in various ways.
